1aad291f092424016a591671ddc52a157c5ebe27
ArKaNeMaN <arkaneman1@gmail.com>
2023-07-12 20:40:07 +0300

    Try to use custom ghpages domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-    base: '/vue3-vipm-wizard/',
+  plugins: [vue({
+      script: {
+          defineModel: true,
+      }
+  })],
 })
